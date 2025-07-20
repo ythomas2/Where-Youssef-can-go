d9cb8fb61e9cebd0cabe939a1fb49372113ed4e6
Youssef Thomas <joeragaa@gmail.com>
2025-07-20 09:16:45 +0300

    add csv for visa data and annotate json with visa requirements

--- map.go
+++ map.go
@@ -1,38 +1,109 @@
 package main
 
-
 import (
-"fmt"
-"os"
-"log"
-"net/http"
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
 )
 
-func getCountries(VisaType string) []string{
-	switch VisaType {
-		case "EVisa":
-			return []string{"thailand","qatar"}
+var visaMap = make(map[string]string) //map the country code to the visa type   
 
-		case "NoVisa":
-			return []string{"kenya","zimbabwe"}
-
-		case "VisaOnEntry":
-			return []string{"indonesia"}
-
-		case "HardVisa":
-			return []string{"USA","canada","mexico"}
-		
-		default:
-			return[]string{"you don messed up aaron"}
-	}
+type FeatureCollection struct {
+	Type     string    `json:"type"` // Should be "FeatureCollection"
+	Features []Feature `json:"features"`
 }
+
+
+// Feature represents a single geographic feature
+type Feature struct {
+	Type       string     `json:"type"`       // e.g., "Feature"
+	ID         string     `json:"id"`         // e.g., "AGO"
+	Properties Properties `json:"properties"` // Nested properties
+	Geometry   Geometry   `json:"geometry"`   // Nested geometry
+}
+
+// Properties holds the specific attributes of the feature
+type Properties struct {
+	Name string `json:"name"` // e.g., "Angola"
+	Visa string `json:"visa"` // e.g., "no"
+}
+
+// Geometry holds the geometric definition of the feature
+type Geometry struct {
+	Type        string        `json:"type"`        // e.g., "MultiPolygon"
+	Coordinates any `json:"coordinates"` // Multi-dimensional array for coordinates
+}
+
+func getVisaMap(){
+	// read csv
+	file,err := os.Open("../passport-index-dataset/passport-index-matrix-iso3.csv")
+	if err!=nil{
+		log.Fatal("couldnt read the countries csv")
+	}
+	defer file.Close()
+	csvReader := csv.NewReader(file)
+	header,err := csvReader.Read()
+	if err!=nil{
+		log.Fatal("couldnt read the countries csv")
+	}
+	for{
+		line,err := csvReader.Read()
+        if err == io.EOF {
+            break
+        }
+        if err != nil {
+            log.Fatal(err)
+        }
+
+		if line[0]=="EGY"{
+			for i := 1; i < len(line); i++ {
+				visaMap[header[i]] = line[i]
+			}
+		}
+	}
+
+	
+	// read the egypt line
+	// populate map variable
+}
+
+
 func MapHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "pages/map.html") // Assuming file.html is in the same directory
 }
 
+func annotateJson(jsonData []byte) []byte {
+	var parsedJson FeatureCollection
+	err := json.Unmarshal(jsonData,&parsedJson)
+	if err !=nil{
+		log.Fatal("could not parse json data into object")
+	}
+
+
+	for i := range parsedJson.Features {
+		visaType,exists := visaMap[parsedJson.Features[i].ID]
+		if exists ==false{
+			fmt.Println("couldnt find in map: ",parsedJson.Features[i].ID)
+		}
+		parsedJson.Features[i].Properties.Visa = visaType 
+	}
+
+	jsonData,err = json.Marshal(parsedJson)
+	if err!=nil{
+		log.Fatal("could not parse json object into data")
+	}
+	// unmarshall data into json object
+	return jsonData
+}
+
+
 func CountriesJsonHandler(w http.ResponseWriter, r *http.Request){
 	jsonFile,err := os.ReadFile("pages/countries.json")
-	
+	jsonFile = annotateJson(jsonFile) // there is probably alot of copying in this implementation
 	if err!=nil{
 		http.Error(w, "Error reading JSON file: "+err.Error(), http.StatusInternalServerError)
 		log.Printf("Error reading countries.json for /api/countries: %v", err)
@@ -44,15 +115,13 @@ func CountriesJsonHandler(w http.ResponseWriter, r *http.Request){
 }
 
 func main() {
-// fileserver := http.FileServer(http.Dir("./pages/"))
-// http.Handle("/", fileserver)
+	getVisaMap()
 
-http.HandleFunc("/", MapHandler)
-http.HandleFunc("/api/countries", CountriesJsonHandler)
+	http.HandleFunc("/", MapHandler)
+	http.HandleFunc("/api/countries", CountriesJsonHandler)
 
-fmt.Printf("port running on http://localhost:8080/\n")
-  if err := http.ListenAndServe(":8080", nil); err != nil{
-    log.Fatal(err)
-  }
+	fmt.Printf("port running on http://localhost:8080/\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil{
+		log.Fatal(err)
+	}
 }
-
